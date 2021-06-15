ead31a6e22c64b77b2d67acead5924abd54e8945
Trevor McCaffrey <tvm36@drexel.edu>
2021-06-15 13:10:48 -0400

    upload example notebook

--- CIVfunctions.py
+++ CIVfunctions.py
@@ -1,46 +1,49 @@
-def trevorFit(data, fit):
-    #Use: Project scattered data (data) onto a line of best fit (fit)
-    #Returns: 2-D array of (x,y) locations on the line of your orthogonal projection
-    
-    #data: 2-D (N by 2) array of your data in some x-y space
-    #fit: 2-D (N by 2) array of coordinates along your line of best fit
-    
-    trevorFit = np.array([]).reshape(0, 2) 
-    
-    for scat in data: 
-        r = np.sqrt((scat[0]-fit[:,0])**2 + (scat[1]-fit[:,1])**2) #dist of data point from each point along fit
-        delta = np.array([fit[np.argmin(r), 0], fit[np.argmin(r), 1]]) #we want to take the index of fit where
-        trevorFit = np.concatenate((trevorFit, np.atleast_2d(delta)))  #the displacement was minimum (min r)
-        
-    return trevorFit
-
-
-def sudo_pca(fit, data, step=1): #add step to decrease runtime for large, finely spaced array
-    #fit: 2-D array containing coordinates of points along best fit line
-    #data: 2-D [x,y] array of data (N by 2 shape)
-    #NOTE: This really just caters to this situation (assumes monotonically decreasing function)
-    
-    data = trevorFit(data, fit) #project data onto fit
-    
-    #
-    if fit[0,1] < fit[-1,1]: #Compare EQW at endpoints [Blueshift, EQW] - want fit[0,1] to be top left of plot
-        fit  = np.flip(fit, axis=0)
-    
-    darr = [] #list to fill with distances for each point
-
-    #Since in this case, the points of the fit (by definition) are already ordered by increasing CIV,
-    #can save a whole lot of time by setting d=0 prior to entering the following loop
-    for scat in data:
-        d = 0 #start at beginning of the line
-        for i in range(0, fit.shape[0]-2, step):
-            xp, x = fit[i,0], fit[i+step-1,0]
-            yp, y = fit[i,1], fit[i+step-1,1] 
-            dp = d
-            d += np.sqrt((x-xp)**2 + (y-yp)**2)
-            if yp >= scat[1] >= y: #if we pass the projected y-coord, save the distance traveled
-                darr.append(d)
-                break
-    
-    return np.array(darr).reshape(len(darr),1)
-
-
+import numpy as np
+from sklearn.preprocessing import scale
+
+def project(data, fit):
+    #Use: Project scattered data (data) onto a line of best fit (fit)
+    #Returns: 2-D array of (x,y) locations on the line of your orthogonal projection
+    
+    #data: 2-D (N by 2) array of your data in some x-y space
+    #fit: 2-D (N by 2) array of coordinates along your line of best fit
+    
+    locs = [] #this will contain xy locations along the best-fit curve corresponding to each data point's orthogonal projection onto said curve - python list faster than numpy array
+    
+    #Loop through each data point and compare with locations along fit
+    for scat in data: 
+        r2 = (scat[0]-fit[:,0])**2 + (scat[1]-fit[:,1])**2      #dist^2 of scat from each point along fit
+        delta = [fit[np.argmin(r2), 0], fit[np.argmin(r2), 1]]  #save point along fit where dist^2 was minimum
+        locs.append(delta)  
+        
+    return np.array(locs)
+
+
+def CIV_distance(data_original, fit_original):
+    #fit: N-by-2 array containing coordinates of points along best fit line 
+    #data: N-by-2 [[x,y]] array of data 
+    #NOTE: This really just caters to this situation (assumes monotonically decreasing fit_original)
+    
+    #1) Scale all the data equally
+    xscale = scale(np.concatenate((fit_original[:,0], data_original[:,0]))) #add ``good`` mask to lofar to remove "bad" reconstructions
+    yscale = scale(np.concatenate((fit_original[:,1], data_original[:,1])))
+    fit = np.array([xscale[0:len(fit_original)], yscale[0:len(fit_original)]]).T
+    data = np.array([xscale[len(fit_original):len(fit_original)+len(data_original)], yscale[len(fit_original):len(fit_original)+len(data_original)]]).T
+    
+    #2) data is now each point's orthogonal projection onto fit
+    data = project(data, fit) 
+        
+    darr = [] #list to fill with distances along best-fit line for each point
+
+    #3) Loop through each data point- start at tip of line and sum dist traveled until passing data point
+    for scat in data:
+        d = 0 #start at beginning of the line
+        for i in range(fit.shape[0]-1):
+            xp, x = fit[i,0], fit[i+1,0]
+            yp, y = fit[i,1], fit[i+1,1] 
+            d += np.sqrt((x-xp)**2 + (y-yp)**2)
+            if yp >= scat[1] > y: #if we pass the projected y-coord, save the distance traveled
+                darr.append(d)
+                break
+                
+    return np.array(darr)
